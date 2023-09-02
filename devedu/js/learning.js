6be6ee7d25e59dbd65120d56b3177b3d5aaae95a
Zahidul-Turja <zahidulturja@gmail.com>
2023-09-02 10:35:02 +0600

    Deploying to Railway

---
+++ devedu/js/learning.js
@@ -0,0 +1,76 @@
+const video = document.querySelector("video");
+const source = video.querySelector("source");
+const contents = document.querySelectorAll(".content")
+const title = document.querySelector(".video-title")
+
+const pdf_reader = document.querySelector(".pdf-reader")
+
+let vid_count = 0;
+let pdf_count = 0;
+
+
+contents.forEach((c) => {
+    const url = c.dataset.url
+    const type = url.slice(-3)
+
+    c.addEventListener("click", () => {
+        video.pause();
+        source.src = "";
+        if (type == "mp4") {
+            source.src = url;
+            video.load();
+            video.currentTime = 0
+            video.muted = false;
+            video.play();
+            title.textContent = c.dataset.serial + ". " + c.dataset.title;
+            video.dataset.serial = c.dataset.serial;
+            vid_count += 1;
+        }
+        else {
+            pdf_reader.href = url;
+            pdf_reader.click();
+            video.pause();
+            pdf_count += 1;
+        }
+    })
+
+    if (type == "mp4") {
+        c.textContent = c.textContent + " (video)"
+    }
+    else if (type == "pdf") {
+        c.textContent = c.textContent + " (pdf)"
+    }
+
+})
+
+
+// ! -------------------------- Course Type ----------------------------
+// ! -------------------------- Certificate ----------------------------
+// const certificate = document.getElementById("certificate")
+// console.log(certificate.href)
+
+// ! -------------------------- Session Video ----------------------------
+
+while(true) {
+    var contents_container = document.querySelector(".contents__container")
+    var session_content = contents_container.querySelector(`[data-serial="${video.dataset.serial}"]`)
+
+    var item_type = session_content.dataset.url.slice(-3)
+
+    if(item_type != "mp4"){
+        video.dataset.serial -= 1;
+        video.dataset.duration = 0
+    }
+    else {
+        video.pause();
+        source.src = session_content.dataset.url;
+        video.load();
+        video.currentTime = video.dataset.duration;
+        // video.play();
+        title.textContent = session_content.dataset.serial + ". " + session_content.dataset.title;
+        video.dataset.serial = session_content.dataset.serial;
+        break;
+    }
+}
+// ! -------------------- Session ends------------------
+
--- e_learning/settings.py
+++ e_learning/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-k_#*13c$iz(4u%&7s%#9ck=!8eaviinyi+%5@q+&k9k(6wsn=$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -102,6 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "devedu"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
