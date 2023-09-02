41837528c742a5e9b88e725a458818145ddd9751
Zahidul-Turja <zahidulturja@gmail.com>
2023-09-02 11:15:37 +0600

    Static_root added in url

--- e_learning/urls.py
+++ e_learning/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("devedu.urls")),
     path("", include("django.contrib.auth.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
